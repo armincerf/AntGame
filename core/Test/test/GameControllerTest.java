e95c018f46d8749bfcd76dd0a57a191768fe47aa
ClayStep <braddpbutcher@gmail.com>
2015-04-15 23:22:27 +0100

    More Testing

--- core/Test/test/GameControllerTest.java
+++ core/Test/test/GameControllerTest.java
@@ -1,9 +1,13 @@
 package test;
 
 import com.AntGame.Controller.AntController;
+import com.AntGame.Controller.Condition;
 import com.AntGame.Controller.GameController;
 import com.AntGame.Controller.MapController;
+import com.AntGame.Model.Ant;
 import com.AntGame.Model.Helper.Colour;
+import com.AntGame.Model.Helper.Direction;
+import com.AntGame.Model.Helper.Position;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -32,9 +36,14 @@ public class GameControllerTest {
 
         String brain = "C:\\Users\\Bradley\\Documents\\GitHub\\AntGame\\core\\assets\\brain1.brain";
         gc.setAntInstructions(brain, brain);
-        mc.createRandomMap();
+        try {
+            mc.createMapFromFile("C:\\Users\\Bradley\\Documents\\GitHub\\AntGame\\core\\assets\\tiny.world");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        
+
+
     }
 
 
@@ -63,25 +72,82 @@ public class GameControllerTest {
     @Test
     public void testCellMatches() throws Exception {
 
+
+        //Place two friendly ants next to each other
+        Ant a1 = new Ant(Position.set(3, 1), Colour.Red, Direction.Right );
+        a1.setHasFood(true); //give a1 food
+        Ant a2 = new Ant(Position.set(4, 1), Colour.Black, Direction.Right );
+        a2.setHasFood(true);
+        mc.setAntAt(a1.getAntPositon(), a1);
+        mc.setMarkerAt(Position.set(5, 1), Colour.Black, 2);
+
+        //Check if sense friend works
+        assertEquals(gc.CellMatches(a1.getAntPositon(), Condition.friend, Colour.Red), true);
+
+        //Check if sense foe works
+        assertEquals(gc.CellMatches(a1.getAntPositon(), Condition.foe, Colour.Black), true);
+
+        //check if friend with food works
+        assertEquals(gc.CellMatches(a1.getAntPositon(), Condition.friendwithfood, Colour.Red), true);
+
+        //check if foe with food works
+        assertEquals(gc.CellMatches(a1.getAntPositon(), Condition.foewithfood, Colour.Black), true);
+
+        //Check food works
+        assertEquals(gc.CellMatches(Position.set(1, 1), Condition.food, Colour.Black), true);
+
+        //check if can sense rocks
+        assertEquals(gc.CellMatches(Position.set(0,0), Condition.rock, Colour.Black), true);
+
+        //Check if can sense markers
+
+        Condition.marker.markerNum = 2;
+
+        assertEquals(gc.CellMatches(Position.set(5,1), Condition.marker , Colour.Black), true);
+
+        //Check if can sense foe markers
+
+        Condition.marker.markerNum = 2;
+
+        assertEquals(gc.CellMatches(Position.set(5,1), Condition.foemarker , Colour.Red), true);
+
+        //Check if can sense home ant hill
+
+        assertEquals(gc.CellMatches(Position.set(1, 2), Condition.home, Colour.Red), true);
+
+        //Check if can sense foe ant hill
+
+        assertEquals(gc.CellMatches(Position.set(1, 2), Condition.foehome, Colour.Black), true);
+
     }
 
     @Test
     public void testGetScore() throws Exception {
+        assertEquals(gc.getScore(Colour.Black), 0);
 
     }
 
     @Test
     public void testGetMoves() throws Exception {
-
+        assertEquals(gc.getMoves(), 0);
     }
 
     @Test
     public void testRandomInt() throws Exception {
-
+        //TODO:WRITE RANDOM INT
     }
 
     @Test
     public void testStep() throws Exception {
+        Ant a1 = new Ant(Position.set(3, 1), Colour.Red, Direction.Right );
+
+        int curmoves = gc.getMoves();
+
+        //Call Step Method
+        gc.step(a1.getID());
+
+        // Check if step method worked by if moves have increased
+        assertEquals(gc.getMoves(), curmoves + 1);
 
     }
 }
